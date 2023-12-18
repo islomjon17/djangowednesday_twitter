b373e38e9f36a6ee3817a7255c66ff0f29340d88
islomjon17 <turgunovislomjon107@gmail.com>
2023-12-18 15:00:13 +0500

    updates

---
+++ twitterclone/urls.py
@@ -0,0 +1,24 @@
+
+
+from django.urls import path, include
+from .  import views
+urlpatterns = [
+    path('', views.home, name="home"),
+    
+    path('profile_list/', views.profile_list, name="profile_list"),
+    #profile url
+    path('profile/<int:pk>', views.profile, name="profile"),
+    
+    path('login', views.login_user, name='login'),
+    
+    path('logout', views.logout_user, name='logout'),
+    
+    # path(),
+    
+    # path(),
+    
+    # path(),
+    
+     
+    
+]
