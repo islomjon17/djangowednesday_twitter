eaaf9cfe534728c62dc94d3e00ddcb580f7c455a
islomjon17 <turgunovislomjon107@gmail.com>
2024-01-08 10:14:39 +0500

    updated urls commit

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -4,20 +4,22 @@ from django.urls import path, include
 from .  import views
 urlpatterns = [
     path('', views.home, name="home"),
-    
+    # profile url
     path('profile_list/', views.profile_list, name="profile_list"),
     #profile url
     path('profile/<int:pk>', views.profile, name="profile"),
-    
+    # login url
     path('login', views.login_user, name='login'),
-    
+    # logout url
     path('logout', views.logout_user, name='logout'),
 
-    
+    # register user url
     path('register_user', views.register_user, name='register'),
+    # update url
     path('update_user', views.update_user, name='update_user'),
-    # path(),
+    # updates like url
     path('update_likes/<int:pk>/', views.update_likes, name='update_likes'),
+    # post share url
     path('post_share/<int:pk>/', views.post_share, name='post_share'),
     
      
